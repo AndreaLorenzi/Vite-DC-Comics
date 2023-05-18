d3980eb20b9532400ab3dbfe68952c78cea7bcb8
Your Name <you@example.com>
2023-05-18 16:06:22 +0200

    first commit

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,6 +2,24 @@
 export default {};
 </script>
 
-<template></template>
+<template>
+  <header>
+    <div>
+      <img src="../assets/img/dc-logo.png" alt="" />
+    </div>
+    <div>
+      <div class="characters">characters</div>
+      <div class="comics">comics</div>
+      <div class="movies">movies</div>
+      <div class="tv">tv</div>
+      <div class="games">games</div>
+      <div class="collectibles">collectibles</div>
+      <div class="video">video</div>
+      <div class="fans">fans</div>
+      <div class="news">news</div>
+      <div class="shop">shop</div>
+    </div>
+  </header>
+</template>
 
 <style></style>
