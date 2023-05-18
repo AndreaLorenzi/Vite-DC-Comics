c37bc62f091f2a2fdadf4cf2c350c5b739488da9
Your Name <you@example.com>
2023-05-18 15:58:32 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppProject from "./components/AppProject.vue";
+import AppFooter from "./components/AppFooter.vue";
+export default {
+  components: {
+    AppHeader,
+    AppProject,
+    AppFooter,
+  },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <AppProject />
+  <AppFooter />
+</template>
+
+<style></style>
